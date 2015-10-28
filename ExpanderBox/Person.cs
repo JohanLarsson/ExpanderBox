7ba8f4c4aac8356ca51ccc78512e5575fbbb3dfe
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-28 20:52:39 +0100

    Initial

---
+++ ExpanderBox/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ExpanderBox
+{
+    public class Person
+    {
+        public Person(string firstName, string lastname)
+        {
+            FirstName = firstName;
+            Lastname = lastname;
+        }
+
+        public string FirstName { get; }
+
+        public string Lastname { get; }
+    }
+}
